a403ae857fbfa904001f8e176637c428800b3316
Bennett Marra <bmarra@email.sc.edu>
2021-04-07 23:41:04 -0400

    Can't crouch while under stuff

--- Assets/Scripts/Crouch.cs
+++ Assets/Scripts/Crouch.cs
@@ -4,6 +4,10 @@ public class Crouch : MonoBehaviour
 {
     CharacterController controller;
     bool isCrouching = false;
+    public Transform headCheck;
+    public LayerMask defaultMask;
+    public float headDistance = 0.4f; // radius of sphere
+    bool isUnder;
     float origincalHeight;
     [SerializeField] float crouchedHeight = 0.5f;
     public KeyCode crouchKey = KeyCode.LeftControl;
@@ -17,9 +21,20 @@ public class Crouch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Are we under something
+        if(Physics.CheckSphere(headCheck.position, headDistance, defaultMask)) {
+            isUnder = true;
+        } else {
+            isUnder = false;
+        }
+
         if(Input.GetKeyDown(crouchKey)) {
+            if (isUnder && isCrouching) {
+                return;
+            } else {
             isCrouching = !isCrouching;
             checkCrouch();
+            }
         }
     }
 
